d65335e82134f9b3f18812bf4e843342751b5526
최영교 <mould837@naver.com>
2021-08-05 23:18:42 +0900

    0805 mysite5 - file upload(galleryDao)

---
+++ src/main/java/com/javaex/dao/GalleryDao.java
@@ -0,0 +1,26 @@
+package com.javaex.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.javaex.vo.GalleryVo;
+
+@Repository
+public class GalleryDao {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public int insert(GalleryVo galleryVo) {
+		
+		return sqlSession.insert("gallery.insert", galleryVo);
+	}
+	
+	//리스트 가져오기
+	public List<GalleryVo> selectList(){
+		return sqlSession.selectList("gallery.selectList");
+	}
+}
